c3448f9d1a34fd184d3de5145530cba6af450e75
Curi <isaac.apoco@gmail.com>
2025-06-17 20:42:05 -0600

    movimiento

--- Assets/Scripts/PlayerInputMovement.cs
+++ Assets/Scripts/PlayerInputMovement.cs
@@ -6,15 +6,29 @@ public class PlayerInputMovement : MonoBehaviour
     public float moveSpeed = 5f;
     private Vector2 moveInput;
     private Rigidbody2D rb;
+    private Animator animator;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
+    // Este método se llama automáticamente por Send Messages
     public void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
+
+        // Solo activa animaciones si hay movimiento
+        bool isMoving = moveInput.sqrMagnitude > 0.01f;
+        animator.SetBool("IsMoving", isMoving);
+
+        // Dirección (opcional si quieres orientar)
+        if (isMoving)
+        {
+            animator.SetFloat("MoveX", moveInput.x);
+            animator.SetFloat("MoveY", moveInput.y);
+        }
     }
 
     void FixedUpdate()
